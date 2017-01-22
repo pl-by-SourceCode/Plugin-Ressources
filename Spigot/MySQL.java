1eee180133c670d111632fc3eff11840edcb425b
pl-by-SourceCode <pl.by.sourcecode@gmail.com>
2017-01-22 09:54:52 +0100

    Update MySQL.java

--- Spigot/MySQL.java
+++ Spigot/MySQL.java
@@ -64,7 +64,7 @@ public class MySQL {
 	}
 	
 	public void createTable(String tablename) throws SQLException {
-		PreparedStatement ps = getConnection().prepareStatement("CREATE TABLE IF NOT EXISTS ? (VARCHAR(100), VHARCAHR(100))");
+		PreparedStatement ps = getConnection().prepareStatement("CREATE TABLE IF NOT EXISTS ? (value1 VARCHAR(100), value2 VHARCAHR(100))");
 		try {
 			ps.setString(1, tablename);
 			ps.executeUpdate();
